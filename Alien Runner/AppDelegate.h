a0d9882a795cbb4f7a88c6ef5709025934116fa8
kenwilcox <wilcoxk@gmail.com>
2014-12-13 22:56:14 -0700

    Initial Project

---
+++ Alien Runner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alien Runner
+//
+//  Created by Kenneth Wilcox on 12/13/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

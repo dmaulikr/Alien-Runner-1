98d3d5885c123c559975398f3122574570bdee16
kenwilcox <wilcoxk@gmail.com>
2014-12-13 23:15:00 -0700

    Setup camera, allow for movement

--- Alien Runner/GameScene.m
+++ Alien Runner/GameScene.m
@@ -12,7 +12,7 @@
 @interface GameScene()
 
 @property (nonatomic) JSTileMap *map;
-
+@property (nonatomic) SKNode *camera;
 @end
 
 @implementation GameScene
@@ -27,8 +27,22 @@
     self.map = [JSTileMap mapNamed:@"Level1.tmx"];
     [self addChild:self.map];
     
+    // Setup camera
+    self.camera = [SKSpriteNode spriteNodeWithColor:[SKColor blackColor] size:CGSizeMake(5, 5)];
+    self.camera.position = CGPointMake(size.width * 0.5, size.height * 0.5);
+    [self.map addChild:self.camera];
   }
   return self;
 }
 
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  for (UITouch *touch in touches) {
+    CGPoint touchLocation = [touch locationInNode:self];
+    CGPoint previousTouchLocation = [touch previousLocationInNode:self];
+    CGPoint movement = CGPointMake(touchLocation.x - previousTouchLocation.x,
+                                   touchLocation.y - previousTouchLocation.y);
+    self.camera.position = CGPointMake(self.camera.position.x + movement.x, self.camera.position.y + movement.y);
+  }
+}
 @end

0edfc7fec6002b8e67e4ef9b963268b65bad0f9a
kenwilcox <wilcoxk@gmail.com>
2014-12-16 00:02:31 -0700

    Added Constants

---
+++ Alien Runner/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  Alien Runner
+//
+//  Created by Kenneth Wilcox on 12/15/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString *const kHighestUnlockedLevel;
+extern NSString *const kSelectedLevel;
+
+@end
---
+++ Alien Runner/Constants.m
@@ -0,0 +1,16 @@
+//
+//  Constants.m
+//  Alien Runner
+//
+//  Created by Kenneth Wilcox on 12/15/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString *const kHighestUnlockedLevel = @"HighestUnlockedLevel";
+NSString *const kSelectedLevel = @"SelectedLevel";
+
+@end
